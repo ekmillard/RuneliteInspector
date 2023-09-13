2e2d157182f01e7e8838a8445afc70eb601a7048
Ethan Ave <ethan@pea2nuts.ru>
2023-09-13 17:26:49 -0400

    Add support for accounts linked to Jagex Launcher. Jagex Launcher accounts can only login using the launcher.

--- src/RuneliteInspectorClient.java
+++ src/RuneliteInspectorClient.java
@@ -1,19 +1,31 @@
 import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+import java.util.Properties;
+
 /**
- * @author Ethan Millard
- * @see <a href="https://www.rune-server.ee/members/264022-akimbo-azure/">Rune-Server profile</a>}
+ * Simple launcher for the RuneLite Event Inspector maintained by
+ * Polar <a href="https://github.com/Joshua-F">(Github)</a> and Kris <a href="https://github.com/z-kris">(Github)</a>
+ *
+ * See README for instructions on using the Jagex Launcher
+ *
+ * @author Ethan Ave {@literal <github.com/e-ave>}
+ * @author Ethan Millard {@literal <github.com/ekmillard>}
+ * @see <a href="https://github.com/ekmillard/RuneliteInspector">Forked from</a>
  */
 public class RuneliteInspectorClient {
 
     private static final String DOWNLOAD_URL = "https://media.z-kris.com/runelite-event-inspector-client.jar";
+    private static final String RL_DIR = System.getProperty("user.home") + File.separator + ".runelite";
     private static final String DOWNLOAD_DIR = System.getProperty("user.home") + File.separator + ".runelite_inspector_clients";
     private static final String FILENAME = "RuneLite.jar";
+    private static final String[] LAUNCHER_VARS = {"JX_ACCESS_TOKEN", "JX_REFRESH_TOKEN", "JX_SESSION_ID", "JX_CHARACTER_ID", "JX_DISPLAY_NAME"};
 
     public static void main(String[] args) {
         try {
@@ -98,12 +110,79 @@ public class RuneliteInspectorClient {
         }
     }
 
+    public static void setEnvironmentVariables(Map<String, String> env) {
+        Properties props = getLauncherVars();
+        for (String varName : LAUNCHER_VARS) {
+            String value = getLauncherVar(props, varName);
+            if(!value.isEmpty()) {
+                env.put(varName, value);
+                System.out.println("Set environment variable " + varName + "=" + value+" before launching client.");
+            }
+        }
+
+    }
+
+    public static String getLauncherVar(Properties launcherVariables, final String s) {
+        String property = System.getenv().get(s);
+        if (property == null) {
+            property = launcherVariables.getProperty(s);
+        }
+        return property;
+    }
+
+    /**
+     * Method from the RuneLite injected client that loads the credentials properties from the .runelite home directory.
+     * The credentials are later set to the environment variables that the client uses to log in from the JX launcher.
+     *
+     * @return The Properties table containing the login information.
+     */
+    public static Properties getLauncherVars() {
+        File credentialsFile = new File(RL_DIR + File.separator, System.getProperty("runelite.credentials.path", "credentials.properties"));
+        Properties launcherVariables = new Properties();
+        if (launcherVariables.isEmpty() && credentialsFile.exists()) {
+            try {
+                final FileInputStream fileInputStream = new FileInputStream(credentialsFile);
+                try {
+                    final InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+                    try {
+                        launcherVariables.load(inputStreamReader);
+                        inputStreamReader.close();
+                    } catch (Throwable t) {
+                        try {
+                            inputStreamReader.close();
+                        }
+                        catch (Throwable t2) {
+                            t.addSuppressed(t2);
+                        }
+                        throw t;
+                    }
+                    fileInputStream.close();
+                } catch (Throwable t3) {
+                    try {
+                        fileInputStream.close();
+                    } catch (Throwable t4) {
+                        t3.addSuppressed(t4);
+                    }
+                    throw t3;
+                }
+            } catch (IOException ex) {
+                System.err.println("unable to load credentials from disk"+ ex);
+            }
+            if (launcherVariables.size() > 0) {
+                System.out.println("read "+launcherVariables.size()+" credentials from disk");
+            }
+        }
+        return launcherVariables;
+    }
+
     private static void runJar() throws IOException {
-        System.out.println("Running JAR file...");
 
         Path path = Paths.get(DOWNLOAD_DIR, FILENAME);
+        System.out.println("Running JAR file... " + path.toString());
         ProcessBuilder pb = new ProcessBuilder("java", "-jar", path.toString());
+        setEnvironmentVariables(pb.environment());
         pb.directory(new File(DOWNLOAD_DIR));
+        System.out.println("Starting client...");
         pb.start();
     }
 }
